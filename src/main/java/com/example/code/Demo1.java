f5796b8aaab948230b51f52d3ca813c2cab1b5e9
xuyangyang <1425809544@qq.com>
2020-05-25 09:55:05 +0800

    fix： 测试javac编译

---
+++ src/main/java/com/example/code/Demo1.java
@@ -0,0 +1,12 @@
+package com.example.code;
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+        int x = 500;
+        int y = 100;
+        int a = x / y;
+        int b = 50;
+        System.out.println(a + b);
+    }
+}
