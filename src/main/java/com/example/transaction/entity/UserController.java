cc0aeec8440c366b5bae0ee420bbcd888f6817c4
xuyangyang <1425809544@qq.com>
2020-05-26 10:53:02 +0800

    事务生效和事务异常不回滚

--- src/main/java/com/example/transaction/entity/UserController.java
+++ src/main/java/com/example/transaction/entity/UserController.java
@@ -1,5 +1,6 @@
 package com.example.transaction.entity;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 
+@Slf4j
 @RestController
 public class UserController {
 
@@ -23,16 +25,22 @@ public class UserController {
         return userService.createUserWrong2(name);
     }
 
-    @GetMapping("wrong3")
-    public int wrong3(@RequestParam("name") String name) {
-        return userService.createUserWrong3(name);
+
+    @GetMapping("right2")
+    public int right2(@RequestParam("name") String name) {
+        try {
+            userService.createUserPublic(new UserEntity(name));
+        } catch (Exception e) {
+            log.error("create user fail", e.getMessage());
+        }
+        return userService.getUserCount(name);
     }
 
     public static void main(String[] args) {
         String dirPath = "E:/uinv_git/workspace/dcv/dcv-web/src/main/webapp/rsm/cli/read/GalileoResource";
         File dir = new File(dirPath);
         System.out.println(dir.isDirectory());
-        if(!dir.isDirectory() && !dir.mkdirs()) {
+        if (!dir.isDirectory() && !dir.mkdirs()) {
             System.out.println("d");
         }
     }
--- src/main/java/com/example/transaction/entity/UserService.java
+++ src/main/java/com/example/transaction/entity/UserService.java
@@ -3,9 +3,17 @@ package com.example.transaction.entity;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.transaction.Transactional;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
+/**
+ *
+ */
 @Slf4j
 @Service
 public class UserService {
@@ -14,10 +22,11 @@ public class UserService {
     private UserRepository userRepository;
 
     @Autowired
-    private UserService userService;
+    private UserService self;
 
     /**
-     * 一个公共方法供controller调用
+     * 1、spring 默认通过动态代理的方式实现 AOP,对目标方法进行增强，private 方法无法被代理到，
+     * spring 自然也无法动态增强事务处理逻辑
      *
      * @param name
      * @return
@@ -40,9 +49,8 @@ public class UserService {
     }
 
 
-
     /**
-     * 一个公共方法供controller调用
+     * 2、必须通过代理过的类从外部调用目标方法才能生效
      *
      * @param name
      * @return
@@ -55,6 +63,27 @@ public class UserService {
         }
         return userRepository.findByName(name).size();
     }
+
+
+    /**
+     * spring 通过 cglb 通过继承方式实现，private 在子类中不可见，自然无法进行事务增强
+     * this 代表对象自己，Spring 不可能注入 this，所以通过this方法访问的必然不是代理
+     *
+     * @param name
+     * @return
+     */
+    public int createUserRight(String name) {
+        try {
+            //this 代表对象自己，Spring 不可能注入 this，所以通过this方法访问的必然不是代理
+//            this.createUserPublic(new UserEntity(name));
+            //spring 中 CGLIB 通过继承方式实现，private 在子类中不可见，自然无法进行事务增强
+            self.createUserPublic(new UserEntity(name));
+        } catch (Exception ex) {
+            log.error("create user failed becauser {} ", ex.getMessage());
+        }
+        return userRepository.findByName(name).size();
+    }
+
     @Transactional
     public void createUserPublic(UserEntity userEntity) {
         userRepository.save(userEntity);
@@ -63,19 +92,52 @@ public class UserService {
         }
     }
 
-    /**
-     * 一个公共方法供controller调用
-     *
-     * @param name
-     * @return
-     */
-    public int createUserWrong3(String name) {
-        try {
-            userService.createUserPrivate(new UserEntity(name));
-        } catch (Exception ex) {
-            log.error("create user failed becauser {} ", ex.getMessage());
+    public int getUserCount(String name) {
+        List<UserEntity> userEntities = userRepository.findByName(name);
+        if (ObjectUtils.isEmpty(userEntities)) {
+            return 0;
         }
-        return userRepository.findByName(name).size();
+
+        return userEntities.size();
     }
+
+    /**
+     * 事务异常不回滚 1、异常必须抛出被捕获，或者手动设置回滚
+     *
+     * @param userEntity
+     */
+    @Transactional
+    public void createUserPublic1(UserEntity userEntity) {
+        try {
+            userRepository.save(userEntity);
+            throw new RuntimeException("error");
+        } catch (Exception ex) {
+            log.error("create user fail ", ex.getMessage());
+
+            //手动设置回滚，否则事务不生效
+//            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+        }
+
+    }
+
+
+    /**
+     * 事务异常不回滚 2、默认捕获 RuntimeException 和 Error ,
+     * 如果捕获 CheckedException 需要通过注解设置 rollbackFor=Exception.class
+     *
+     * @param userEntity
+     * @throws IOException
+     */
+//    @Transactional(rollbackOn = Exception.class)
+    @Transactional
+    public void createUserPublic2(UserEntity userEntity) throws IOException {
+        userRepository.save(userEntity);
+        ortherTask();
+    }
+
+    private void ortherTask() throws IOException {
+        Files.readAllLines(Paths.get("file-not-exist"));
+    }
+
 
 }
