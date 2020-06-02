eca5eff841c09851bfbb5c254f7a13ec2cf949f2
xuyangyang <1425809544@qq.com>
2020-06-02 15:23:06 +0800

    fix: 两种方式判断线程池停止

--- src/main/java/com/example/thread/threadlocal/scene1/ThreadLocalDemo08.java
+++ src/main/java/com/example/thread/threadlocal/scene1/ThreadLocalDemo08.java
@@ -3,21 +3,16 @@ package com.example.thread.threadlocal.scene1;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
  * 加 ThreadLocal 解决
- * <p>
- * {@link ThreadLocal} 用作保存每个线程独享的对象，为每个线程都创建一个副本，每个线程都只能修改自己所拥有的副本,
- * 而不会影响其他线程的副本，这样就让原本在并发情况下，线程不安全的情况变成了线程安全的情况。
- * * 关闭线程池方法 {@link CountDownLatch await}
- *
+ * 关闭线程池方法 {@link ExecutorService awaitTermination}
  * @author xuyy
  */
-public class ThreadLocalDemo06 {
+public class ThreadLocalDemo08 {
 
     public static ExecutorService threadPoll = new ThreadPoolExecutor(16, 16,
             0L, TimeUnit.MILLISECONDS,
@@ -28,18 +23,19 @@ public class ThreadLocalDemo06 {
 
     public static void main(String[] args) throws InterruptedException {
         long st = System.currentTimeMillis();
-        CountDownLatch countDownLatch = new CountDownLatch(1000);
         for (int i = 0; i < 1000; i++) {
             int finalI = i;
             threadPoll.execute(() -> {
                 String date = date(finalI);
                 System.out.println(date);
-                countDownLatch.countDown();
             });
         }
-        countDownLatch.await();
-        long et = System.currentTimeMillis();
-        System.out.println("耗时："+(et-st)+"秒");
+        threadPoll.shutdown();
+        threadPoll.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        if (threadPoll.isShutdown()) {
+            long et = System.currentTimeMillis();
+            System.out.println("耗时：" + (et - st) + "秒");
+        }
     }
 
 
@@ -54,7 +50,7 @@ public class ThreadLocalDemo06 {
         public static ThreadLocal<SimpleDateFormat> simpleDateFormatThreadLocal = new ThreadLocal<SimpleDateFormat>() {
             @Override
             protected SimpleDateFormat initialValue() {
-                return new ThreadLocalDemo06().simpleDateFormat;
+                return new ThreadLocalDemo08().simpleDateFormat;
             }
 
         };
